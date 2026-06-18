2455fd87d42a1e739cd2bf20709669918758b7ee
WANNORAMIRA <m-9024124@moe-dl.edu.my>
2026-06-18 09:17:07 +0800

    Update hello_ai.py

--- hello_ai.py
+++ hello_ai.py
@@ -1,3 +1,4 @@
-Welcome to Computer Vision Playground
-This repository is used for learning Github workflow.
-AI and Computer Vision are exciting technologies!
+print ("Welcome to Computer Vision Playground!")
+print ("This repository is used for learning Github workflow.")
+print ("AI and Computer Vision are exciting technologies!")
+
